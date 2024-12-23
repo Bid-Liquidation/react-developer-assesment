a66d6178d5defad29b7e73a73d0f51fe746759fe
ehsafin <safin@studiodevhub.com>
2024-12-23 16:32:31 +0600

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>React Developer Assesment</h1>
+
+      <p style={{ fontSize: 24 }}>
+        Welcome to the React Developer Assesment! Please follow the instructions
+        in <span style={{ fontWeight: 700 }}>README.md</span> to complete the
+        task.
+      </p>
+    </>
+  );
+}
+
+export default App;
